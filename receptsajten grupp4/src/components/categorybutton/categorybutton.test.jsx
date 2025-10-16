98dfa598e896f82292939aadd8e14a08a721f020
David Izquierdo Puentes <david_izqpue@hotmail.com>
2025-10-16 21:11:09 +0200

    added code to check name on button

--- receptsajten grupp4/src/components/categorybutton/categorybutton.test.jsx
+++ receptsajten grupp4/src/components/categorybutton/categorybutton.test.jsx
@@ -2,5 +2,5 @@ import {render,screen, fireEvent} from '@testing-library/react'
 import CategoryButton from './categorybutton'
 
 test('renders button w/ correct name',()=>{
-
+    render(<CategoryButton name="Gindrinkar" onClick={() => {}} />)
 });
