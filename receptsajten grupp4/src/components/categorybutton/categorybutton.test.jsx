b1102c4980992e0fafd3e84b51069ae8b00048bb
David Izquierdo Puentes <david_izqpue@hotmail.com>
2025-10-16 21:36:21 +0200

    added import vitest

--- receptsajten grupp4/src/components/categorybutton/categorybutton.test.jsx
+++ receptsajten grupp4/src/components/categorybutton/categorybutton.test.jsx
@@ -1,5 +1,6 @@
 import {render,screen, fireEvent} from '@testing-library/react'
 import CategoryButton from './categorybutton'
+import {test,expect} from 'vitest'
 
 test('renders button w/ correct name',()=>{
     render(<CategoryButton name="Gindrinkar" onClick={() => {}} />)
