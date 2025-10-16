bdd6c53d467c5bb7cedd2781aeb47ff741184212
David Izquierdo Puentes <david_izqpue@hotmail.com>
2025-10-16 21:25:18 +0200

    added assertion to verify button exists in document

--- receptsajten grupp4/src/components/categorybutton/categorybutton.test.jsx
+++ receptsajten grupp4/src/components/categorybutton/categorybutton.test.jsx
@@ -4,4 +4,5 @@ import CategoryButton from './categorybutton'
 test('renders button w/ correct name',()=>{
     render(<CategoryButton name="Gindrinkar" onClick={() => {}} />)
     const button = screen.getByText('Gindrinkar')
+    expect(button).toBeInTheDocument();
 });
