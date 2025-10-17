3d0d656b511cb9cbd1337096f86aa1f100eb004d
David Izquierdo Puentes <david_izqpue@hotmail.com>
2025-10-17 13:05:24 +0200

    Add testing dependencies and update CategoryButton tests
    - Added @testing-library/jest-dom and @testing-library/react to devDependencies for enhanced testing capabilities.
    - Updated CategoryButton test to include BrowserRouter for proper routing context and verified button rendering with correct name.

--- receptsajten grupp4/src/components/categorybutton.test.jsx
+++ receptsajten grupp4/src/components/categorybutton.test.jsx
@@ -1,8 +1,13 @@
-import {render,screen, fireEvent} from '@testing-library/react'
-import CategoryButton from './categorybutton'
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import CategoryButton from './categorybutton';
 
-test('renders button w/ correct name',()=>{
-    render(<CategoryButton name="Gindrinkar" onClick={() => {}} />)
-    const button = screen.getByText('Gindrinkar')
-    expect(button).toBeInTheDocument();
+test('renders button w/ correct name', () => {
+  render(
+    <BrowserRouter>
+      <CategoryButton name="Gindrinkar" isActive={false} />
+    </BrowserRouter>
+  );
+  const button = screen.getByText('Gindrinkar');
+  expect(button).toBeInTheDocument();
 });
